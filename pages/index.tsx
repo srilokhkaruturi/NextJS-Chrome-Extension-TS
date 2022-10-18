bfee492878d7ee19ca654794f3f692e6978e2d1f
sai-k02 <srilokh.karuturi@gmail.com>
2022-10-18 16:35:48 -0500

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <section>
+        <h1>Hello!</h1>
+        <p>This is a Next.js application. 😎</p>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
